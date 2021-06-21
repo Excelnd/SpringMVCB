76576c9b5b71c5f485b0e214a173c34cff694a54
Admin i <Admin i@DESKTOP-F5BCCST>
2021-06-21 12:59:42 +0530

    Add Customer Annotation CodeConstarintValidator

---
+++ src/com/ihs/springd/mvc/validation/CourseCode.java
@@ -0,0 +1,28 @@
+package com.ihs.springd.mvc.validation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD } ) 
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+
+	// define default course code
+	public String value() default "IHS";
+	
+	// define default error message
+	public String message() default "must start with IHS";
+	
+	// define default groups
+	public Class<?>[] groups() default {};
+	
+	// define default payloads
+	public Class<? extends Payload>[] payload() default {};
+	
+}
